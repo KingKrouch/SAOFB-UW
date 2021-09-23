0b37d83090cfa7d59cdc1413195d82169648c2c5
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2021-09-23 01:50:12 -0400

    Initial Commit

---
+++ Binaries/Win64/plugins/debug_debug_config.bat
@@ -0,0 +1,15 @@
+copy ".\SAOFBUW_Debug_Win64.asi" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\SAOFBUW_Debug_Win64.ilk" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\SAOFBUW_Debug_Win64.pdb" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\config.ini" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\updater.exe" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\Updater.ilk" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\Updater.pdb" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+start steam://run/626690
