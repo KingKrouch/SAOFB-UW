8b2428c8b49172c3b281f08952e488f0ab675835
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2021-09-23 01:34:01 -0400

    Removed Updater mentions, moved from IniPP to IniReader, Organized VS Solution

---
+++ Binaries/Win64/plugins/debug_release_config.bat
@@ -0,0 +1,7 @@
+copy ".\SAOFBUW_Release_Win64.asi" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\config.ini" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+copy ".\updater.exe" "Z:\Steam\steamapps\common\SWORD ART ONLINE FATAL BULLET\SAOFB\Binaries\Win64\plugins"
+
+start steam://run/626690
